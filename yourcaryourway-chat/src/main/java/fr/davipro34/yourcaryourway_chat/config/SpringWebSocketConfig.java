8cda3be9f4be1b3c4fddada0dd62dd4249ebf542
David <david@davipro.fr>
2024-08-21 09:05:06 +0200

    Spring WebSocket configured.

---
+++ yourcaryourway-chat/src/main/java/fr/davipro34/yourcaryourway_chat/config/SpringWebSocketConfig.java
@@ -0,0 +1,23 @@
+package fr.davipro34.yourcaryourway_chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SpringWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
