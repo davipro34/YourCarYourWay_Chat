c67627712904265aeb6d7e226033345b976500fe
David <david@davipro.fr>
2024-08-21 08:58:04 +0200

    Spring Boot project initialized.

---
+++ yourcaryourway-chat/src/main/java/fr/davipro34/yourcaryourway_chat/YourcaryourwayChatApplication.java
@@ -0,0 +1,13 @@
+package fr.davipro34.yourcaryourway_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourcaryourwayChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourcaryourwayChatApplication.class, args);
+	}
+
+}
